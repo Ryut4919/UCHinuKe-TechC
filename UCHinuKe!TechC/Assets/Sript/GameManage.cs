c17101fe8214f15c8906f038ec72eb84139fd49e
Ryut4919 <tzhw.lai@gmail.com>
2018-12-14 14:41:12 +0900

    Plus
    操作説明書を追加、
    ゲームー終了時、自動的にウィンドウ終了

--- UCHinuKe!TechC/Assets/Sript/GameManage.cs
+++ UCHinuKe!TechC/Assets/Sript/GameManage.cs
@@ -9,67 +9,91 @@ public class GameManage : MonoBehaviour {
     
     //ゲーム失敗信号
     public bool Over;
+    
     //ゲームクリア信号
     public bool Clear;
+
     //ゲーム失敗
     bool _over = false;
+    
     //ものを非表示
     bool Clearall = false;
+
+    //操作説明書を隠す
+    bool PanelClose = false;
 
     [SerializeField]
     private GameObject _PointCount;
 
+    //操作説明書
+    [SerializeField]
+    private GameObject _controlPanel;
+
     public GameObject _pointChecking;
     //最初ゲームスタートのカウントダウン
     public  Text _text;
+    
     //カウントダウン時間
     float timer = 3;
     public GameObject BigBig;
+    
     //スライムの撃破数
     public int Point;
+    
+
+    void Start()
+    {
+        //操作説明書を指定ところへ
+        iTween.MoveTo(_controlPanel, new Vector3(524.0f, 418.0f, 0), 3.0f);
+    }
     
 	// Update is called once per frame
 	void Update () {
         
-        #region Timer
-        timer -= Time.deltaTime;
-        //整数のように表示する
-        _text.text = "" + timer.ToString("0");
-        if (timer < 0)
+        PanelC();
+        if (PanelClose)
         {
-            _text.text = "Start!!!";
-            //BGMをプレイしてない場合
-            if (!GetComponent<AudioSource>().isPlaying)
+                #region Timer
+            timer -= Time.deltaTime;
+            //整数のように表示する
+            _text.text = "" + timer.ToString("0");
+            if (timer < 0)
             {
-                //BGMをプレイする
-                GetComponent<AudioSource>().Play();
+                _text.text = "Start!!!";
+                //BGMをプレイしてない場合
+                if (!GetComponent<AudioSource>().isPlaying)
+                {
+                    //BGMをプレイする
+                    GetComponent<AudioSource>().Play();
+                }
             }
+            if (timer < -1 && !Clearall)
+            {
+                //カウントダウン文字を非表示
+                _text.gameObject.SetActive(false);
+                //ターゲットマークを表示
+                BigBig.SetActive(true);
+                //撃破数を表示
+                _PointCount.SetActive(true);
+            }
+            #endregion
+            #region gameOver
+            //ゲームオーバーの場合
+            if (Over&& !_over)
+            {
+                //gamelose
+                _pointChecking.SetActive(false);
+                //ゲーム失敗
+                _over = true;
+                //ゲームー終了パネルを表示
+                ControlWLScene.SetActive(true);
+                ControlWLScene.transform.GetChild(0).GetComponent<Text>().text = "ゲーム失敗";
+                ClearAll();
+                
+                //Debug.Log("GameOver");
+            }
+            #endregion
         }
-        if (timer < -1 && !Clearall)
-        {
-            //カウントダウン文字を非表示
-            _text.gameObject.SetActive(false);
-            //ターゲットマークを表示
-            BigBig.SetActive(true);
-            //撃破数を表示
-            _PointCount.SetActive(true);
-        }
-        #endregion
-        #region gameOver
-        //ゲームオーバーの場合
-        if (Over&& !_over)
-        {
-            //gamelose
-            _pointChecking.SetActive(false);
-            //ゲーム失敗
-            _over = true;
-            //ゲームー終了パネルを表示
-            ControlWLScene.SetActive(true);
-            ControlWLScene.transform.GetChild(0).GetComponent<Text>().text = "ゲーム失敗";
-            ClearAll();
-            //Debug.Log("GameOver");
-        }
-        #endregion
 
         #region Getpoint点数を取得
         /*else*/
@@ -89,6 +113,19 @@ public class GameManage : MonoBehaviour {
         #endregion
     }
 
+    void PanelC()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1) && !PanelClose)
+        {
+            //操作説明書を見えないところへ
+            iTween.MoveTo(_controlPanel, new Vector3(1550.0f, 418.0f, 0), 3.0f);
+            new WaitForSeconds(1f);
+            //カウントダウンを表示
+            _text.gameObject.SetActive(true);
+            PanelClose = true;
+        }
+    }
+
     void ClearAll()
     {
         Clearall = true;
@@ -96,5 +133,13 @@ public class GameManage : MonoBehaviour {
         BigBig.SetActive(false);
         //撃破数を非表示
         _pointChecking.SetActive(false);
+        StartCoroutine("CloseWindow");
+    }
+
+    IEnumerator CloseWindow()
+    {
+        yield return new WaitForSeconds(6.0f);
+        //6秒後ゲームーを終了する；
+        Application.Quit();
     }
 }
